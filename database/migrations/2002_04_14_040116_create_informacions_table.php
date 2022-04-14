30b16702f449d08d5a7218e5ef484355aa284bc0
MariaInesB7 <mariainesbb2000@gmail.com>
2022-04-14 00:06:51 -0400

    sitio e informacion

---
+++ database/migrations/2002_04_14_040116_create_informacions_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateInformacionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('informacions', function (Blueprint $table) {
+            $table->id();
+            $table->string('ubicacion');
+            $table->string('enlace_foto');
+            $table->foreign('sitio_id')->references('id')->on('sitio')->ondelete('cascade')->onupdate('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('informacions');
+    }
+}
